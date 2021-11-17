36d73a091841d83b4e5422ec36908f83dab43010
Kim A Reum <ksah1016@gmail.com>
2021-11-18 00:51:03 +0900

    service js

--- app.js
+++ app.js
@@ -18,3 +18,143 @@ let greetings = () => {
 }
 
 greetings();
+
+//Bookmark 공통
+
+const actives = document.querySelectorAll('.active');
+
+    for (let i =0; i< actives.length; i++) {
+        actives[i].classList.remove('active');
+        actives[i].addEventListener("click", () => {
+            actives[i].classList.toggle('active');
+        })
+        
+    }
+
+//Melon
+let paints = document.querySelector(".paint");
+let melons = document.getElementById('melon-id');
+let observermelon = new MutationObserver(fnHandlerDocsmelon),
+    elTargetmelon = melons,
+    objConfigmelon = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsmelon() {
+    paints.innerText = '　Melon';
+}
+observermelon.observe(elTargetmelon, objConfigmelon);
+
+//Genie
+let paints2 = document.querySelector(".paint2");
+let genies = document.getElementById('genie-id');
+let observergenie = new MutationObserver(fnHandlerDocsgenie),
+    elTargetgenie = genies,
+    objConfiggenie = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsgenie() {
+    paints2.innerText = '　Genie';
+}
+observergenie.observe(elTargetgenie, objConfiggenie);
+
+//Flo
+let paints3 = document.querySelector(".paint3");
+let flos = document.getElementById('flo-id');
+let observerflo = new MutationObserver(fnHandlerDocsflo),
+    elTargetflo = flos,
+    objConfigflo = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsflo() {
+    paints3.innerText = '　Flo';
+}
+observerflo.observe(elTargetflo, objConfigflo);
+
+//Bugs
+let paints4 = document.querySelector(".paint4");
+let bugss = document.getElementById('bugs-id');
+let observerbugs = new MutationObserver(fnHandlerDocsbugs),
+    elTargetbugs = bugss,
+    objConfigbugs = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsbugs() {
+    paints4.innerText = '　Bugs';
+}
+observerbugs.observe(elTargetbugs, objConfigbugs);
+
+//Vibe
+let paints5 = document.querySelector(".paint5");
+let vibes = document.getElementById('vibe-id');
+let observervibe = new MutationObserver(fnHandlerDocsvibe),
+    elTargetvibe = vibes,
+    objConfigvibe = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsvibe() {
+    paints5.innerText = '　Vibe';
+}
+observervibe.observe(elTargetvibe, objConfigvibe);
+
+//Applemusic
+let paints6 = document.querySelector(".paint6");
+let applemusics = document.getElementById('applemusic-id');
+let observerapplemusic = new MutationObserver(fnHandlerDocsapplemusic),
+    elTargetapplemusic = applemusics,
+    objConfigapplemusic = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsapplemusic() {
+    paints6.innerText = '　Apple music';
+}
+observerapplemusic.observe(elTargetapplemusic, objConfigapplemusic);
+
+//Spotify
+let paints7 = document.querySelector(".paint7");
+let spotifys = document.getElementById('spotify-id');
+let observerspotify = new MutationObserver(fnHandlerDocsspotify),
+    elTargetspotify = spotifys,
+    objConfigspotify = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsspotify() {
+    paints7.innerText = '　Spotify';
+}
+observerspotify.observe(elTargetspotify, objConfigspotify);
+
+//Youtube Music
+let paints8 = document.querySelector(".paint8");
+let youtubemusics = document.getElementById('youtubemusic-id');
+let observeryoutubemusic = new MutationObserver(fnHandlerDocsyoutubemusic),
+    elTargetyoutubemusic = youtubemusics,
+    objConfigyoutubemusic = {
+        childlist: false,
+        subtree: false,
+        attributes: true,
+        CharacterData: false
+    };
+function fnHandlerDocsyoutubemusic() {
+    paints8.innerText = '　Youtube music';
+}
+observeryoutubemusic.observe(elTargetyoutubemusic, objConfigyoutubemusic);
